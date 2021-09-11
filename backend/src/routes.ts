9508847694d28f02a8bfbda17379f63c1fecb98d
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-11 17:03:56 -0300

    update: created detail route and adding validation to create and update routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import TractorController from './controllers/TractorController'
 import multer from 'multer'
 import uploadConfig from './config/multerconfig'
+import { validation } from "./middlewares/validation";
+import { createTractorSchema, updateTractorSchema } from "./yup-schemas/schemas";
 
 const upload = multer(uploadConfig)
 
@@ -9,10 +11,12 @@ const routes = Router()
 
 routes.get('/tractors', TractorController.index)
 
-routes.post('/create/tractor', upload.single('image') ,TractorController.create)
+routes.post('/create/tractor', upload.single('image'), validation(createTractorSchema) ,TractorController.create)
 
-routes.put('/tractors/:id', upload.single('image') , TractorController.update)
+routes.put('/tractors/:id', upload.single('image'), validation(updateTractorSchema) , TractorController.update)
 
 routes.delete('/tractors/:id', TractorController.delete)
+
+routes.get('/tractors/:id', TractorController.detail)
 
 export {routes}
