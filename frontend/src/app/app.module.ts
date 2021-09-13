861b9096a5b8d4a30160158bf42f52ba993b666e
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-13 17:47:18 -0300

    update: added imports for ModalComponent and the feather icons on it

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,19 +7,21 @@ import { HomeComponent } from './pages/home/home.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import {HttpClientModule} from '@angular/common/http'
 import { FeatherModule } from 'angular-feather';
-import {Trash2, Edit} from 'angular-feather/icons';
+import {Trash2, Edit, Plus, X} from 'angular-feather/icons';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ItemListComponent
+    ItemListComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FeatherModule.pick({Trash2, Edit})
+    FeatherModule.pick({Trash2, Edit, Plus, X})
   ],
   providers: [],
   bootstrap: [AppComponent]
