01ed1cc1a16bb6bcf4e46f94652e8e1dda852ff1
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-12 21:23:01 -0300

    add: itemListComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
