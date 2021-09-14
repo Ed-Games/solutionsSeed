59d73838eafda1998d9ae2669c73b1fa24f9c1d2
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-14 09:05:42 -0300

    update: created registerForm and formBuilder to store data from user input and submitForm() method to create a new tractor

--- frontend/src/app/components/modal/modal.component.ts
+++ frontend/src/app/components/modal/modal.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ApiService } from 'src/app/services/apiService';
 
 @Component({
   selector: 'app-modal',
@@ -8,12 +10,26 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class ModalComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ModalComponent>) { }
+  public registerForm : FormGroup = {} as FormGroup;
+
+  private formBuilder: FormBuilder = new FormBuilder;
+
+  constructor(public dialogRef: MatDialogRef<ModalComponent>, private apiService: ApiService) { }
 
   ngOnInit(): void {
+  this.registerForm = this.formBuilder.group({
+      image: [''],
+      name: ['', Validators.required]
+    })
   }
 
   closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  submitForm(): void {
+    this.apiService.createTractor(this.registerForm.value).subscribe(response => {})
+    console.log(this.registerForm.value)
     this.dialogRef.close();
   }
 
