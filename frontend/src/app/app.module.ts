6cc067b4dda1a25deed27882dbfc605a80e31340
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-14 09:04:07 -0300

    update: imported MatInputModule, FormsModule and ReactiveFormsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomeComponent } from './pages/home/home.component'
+import { ItemListComponent } from './components/item-list/item-list.component'
 import {HttpClientModule} from '@angular/common/http'
-import { FeatherModule } from 'angular-feather';
-import {Trash2, Edit, Plus, X} from 'angular-feather/icons';
-import { ModalComponent } from './components/modal/modal.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FeatherModule } from 'angular-feather'
+import {Trash2, Edit, Plus, X} from 'angular-feather/icons'
+import { ModalComponent } from './components/modal/modal.component'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import {MatDialogModule, MatDialogRef} from '@angular/material/dialog'
+import {MatInputModule} from '@angular/material/input'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -25,7 +27,10 @@ import {MatDialogModule, MatDialogRef} from '@angular/material/dialog'
     HttpClientModule,
     FeatherModule.pick({Trash2, Edit, Plus, X}),
     NoopAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
