45f6654e49aec8f91dc0a77536504035a55dce27
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-15 18:21:13 -0300

    update: added dialog methods to display an update dialog when edit icon is clicked

--- frontend/src/app/components/item-list/item-list.component.ts
+++ frontend/src/app/components/item-list/item-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ITractor } from 'src/app/interfaces/ITractor';
 import { ApiService } from 'src/app/services/apiService';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-item-list',
@@ -11,12 +13,22 @@ export class ItemListComponent implements OnInit {
 
   tractors  : ITractor[] = []
 
-  constructor(public apiService : ApiService) {
+  constructor(public apiService : ApiService, public dialog: MatDialog ) {
 
   }
 
   ngOnInit(): void {
     this.getTractors()
+  }
+
+  openDialog(tractor: ITractor): void {
+    const dialogRef = this.dialog.open(ModalComponent,{
+      data: {type :'update', tractor}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
   public getTractors(){
