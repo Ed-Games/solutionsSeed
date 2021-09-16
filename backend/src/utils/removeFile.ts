f8852fd04a5b4b57d6d744c6a6d027da84ea39f6
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-16 10:43:22 -0300

    update: prevented to remove the default image

--- backend/src/utils/removeFile.ts
+++ backend/src/utils/removeFile.ts
@@ -2,7 +2,8 @@ import fs from 'fs'
 import path from 'path'
 
 export async function removeFile(file: string){
-    const filePath = path.join(__dirname, '..', 'uploads', file)
+    if(file=="default.jpg") return
+    const filePath = path.join(__dirname, '..','..', 'uploads', file)
     fs.unlink(filePath, (error)=>{
         if(error) {
             console.log(error)
