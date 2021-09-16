c5803b74be4316deb7bb6ba33e4379a3408e52c2
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-16 08:46:21 -0300

    update: created openConfirmDialog method and calling getTractors when modal closes

--- frontend/src/app/components/item-list/item-list.component.ts
+++ frontend/src/app/components/item-list/item-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ITractor } from 'src/app/interfaces/ITractor';
 import { ApiService } from 'src/app/services/apiService';
+import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
@@ -13,7 +14,7 @@ export class ItemListComponent implements OnInit {
 
   tractors  : ITractor[] = []
 
-  constructor(public apiService : ApiService, public dialog: MatDialog ) {
+  constructor(public apiService : ApiService, public dialog: MatDialog, public confirmDialog : MatDialog) {
 
   }
 
@@ -27,7 +28,21 @@ export class ItemListComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+      console.log("getting data....")
+      this.getTractors()
+      console.log(this.tractors)
+    });
+  }
+
+  openConfirmDialog(id: string|undefined): void {
+    const dialogRef = this.dialog.open(ConfirmModalComponent,{
+      data: {id: id},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("getting data....")
+      this.getTractors()
+      console.log(this.tractors)
     });
   }
 
